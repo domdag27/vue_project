bbd18fc754ad7eb3d0765b3ab87e4a02548241b7
domenick.dagostino <domenick.dagostino@mcgstrategic.com>
2022-10-19 17:42:24 -0400

    Main: Pushing rest of files

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contact-form">
+    <h2>{{ value }}</h2>
+    <input type="text" :value="value" placeholder="">
+    <a href="#" class="submit">Submit</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactForm",
+    props: {
+      value: Number
+  }  
+}
+</script>
+
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contact">
+    <h1>This is a contact page</h1>
+    <ContactForm :value="value"/>
+  </div>
+</template>
+
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+export default {
+  name: "Contact",
+  components: {
+    ContactForm
+  },
+    data() {
+    return {
+      value: 10
+    }
+  }
+}
+</script>
